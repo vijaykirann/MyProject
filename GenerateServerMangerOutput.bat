38c4fa7b734288adddb43d4c08deb6dfcd28478e
vijaykirann <30449746+vijaykirann@users.noreply.github.com>
2017-08-18 10:49:19 +0530

    Add files via upload

---
+++ GenerateServerMangerOutput.bat
@@ -0,0 +1,1 @@
+D:\sba81_PRD\siebsrvr\BIN\srvrmgr.exe /e PRD_ES /g APPAU182FRM208 /u sadmin /p E26Q1r+esrELGRwPn19JfBD/Cyg= /i "C:\Executable\Command.txt"
---
+++ imgs/copy_2ImageFolders.cmd
@@ -0,0 +1,30 @@
+echo DEV02
+xcopy /r /y *.* \\appaudevdt002.appdev.corptst.anz.com\e$\Scripts\dash\imgs
+echo DEV03
+xcopy /r /y *.* \\appau227dev133.appdev.corptst.anz.com\d$\Scripts\dash\imgs
+echo DEV04
+xcopy /r /y *.* \\appau227dev180.appdev.corptst.anz.com\d$\Scripts\dash\imgs
+echo DEV05
+xcopy /r /y *.* \\appau227dev193.appdev.corptst.anz.com\d$\Scripts\dash\imgs
+echo DEV06
+xcopy /r /y *.* \\appau227dev208.appdev.corptst.anz.com\d$\Scripts\dash\imgs
+echo DEV07
+xcopy /r /y *.* \\appau227dev184.appdev.corptst.anz.com\d$\Scripts\dash\imgs
+echo ATE01
+xcopy /r /y *.* \\appau227tst133.appdev.corptst.anz.com\d$\Scripts\dash\imgs
+echo ATE02
+xcopy /r /y *.* \\appau227dev183.appdev.corptst.anz.com\d$\Scripts\dash\imgs
+echo ATE03
+xcopy /r /y *.* \\appau227tst134.appdev.corptst.anz.com\d$\scripts\dash\imgs
+echo ATE04
+xcopy /r /y *.* \\appau227dev181.appdev.corptst.anz.com\d$\Scripts\dash\imgs
+echo ATE05
+xcopy /r /y *.* \\appau227tst152.appdev.corptst.anz.com\d$\scripts\dash\imgs
+echo ATE06
+xcopy /r /y *.* \\appau227tst113.appdev.corptst.anz.com\d$\scripts\dash\imgs
+echo ATE07
+xcopy /r /y *.* \\appau227tst115.appdev.corptst.anz.com\d$\scripts\dash\imgs
+echo ATE08
+xcopy /r /y *.* \\appau182dev60.appdev.corptst.anz.com\d$\scripts\dash\imgs
+echo MREF
+pause
